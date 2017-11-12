e8214d4ba618865f17f6d7f0cb4c921fcdba48af
Gopalakrishnan <fax6@INMACY4AAH.local>
2017-11-12 19:53:26 +0530

    added reactive producer and consumer

---
+++ src/main/java/KafkaConsumer.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by fax6 on 12/11/17.
+ */
+public class KafkaConsumer {
+}
---
+++ src/main/java/KafkaProducer.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by fax6 on 11/11/17.
+ */
+public class KafkaProducer {
+}
